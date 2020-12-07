be665c8ddaaedc570889b6c705f1975a5efa0b9f
NiPeGun <nipegun@gmail.com>
2020-12-07 07:49:17 +0100

    Create CrearRutaDefault.sh

---
+++ CrearRutaDefault.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+#---------------------------------------------
+#  Script de NiPeGun para actualizar OpenWrt
+#---------------------------------------------
+
+#ip route add default via 192.168.0.1
+ip route add default via 192.168.1.1
+
