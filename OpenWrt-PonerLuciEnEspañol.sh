52314b146330938f70587661e9f37f66c1966bb1
NiPeGun <nipegun@gmail.com>
2021-01-02 03:01:09 +0100

    Create OpenWrt-PonerLuciEnEspañol.sh

---
+++ OpenWrt-PonerLuciEnEspañol.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+#------------------------------------------------------------------------
+#  Script de NiPeGun para instalar el paquete de idioma español de LUCI
+#------------------------------------------------------------------------
+
+opkg update
+opkg install luci-i18n-base-es
+opkg install luci-i18n-firewall-es
+
