4f82ad416d8ee3ec8b8df30e8bf16709ace626fd
NiPeGun <nipegun@gmail.com>
2021-01-02 01:10:45 +0100

    Create Rutas-BorrarTodas.sh

---
+++ Rutas-BorrarTodas.sh
@@ -0,0 +1,16 @@
+
+#!/bin/sh
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+#----------------------------------------------------------------------
+#  Script de NiPeGun para borrar todas las tablas de rutas en OpenWrt
+#----------------------------------------------------------------------
+
+ip route flush cache
+ip route flush table main
+ip route flush all                # Puede que no sea necesario
+
