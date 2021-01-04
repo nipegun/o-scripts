cfc7afa16022abfbfe4986c1d3ce2722ca8f9438
NiPeGun <nipegun@gmail.com>
2021-01-04 08:17:50 +0100

    Update WireGuard-InstalarYConfigurar.sh

--- SoftInst/WireGuard-InstalarYConfigurar.sh
+++ SoftInst/WireGuard-InstalarYConfigurar.sh
@@ -68,8 +68,7 @@ uci set network.wg0="interface"
 uci set network.wg0.proto="wireguard"
 uci set network.wg0.private_key="$(cat /root/WireGuard/WireGuardServerPrivate.key)"
 uci set network.wg0.listen_port="51820"
-uci add_list network.wg0.addresses='<A /64 (or greater) IPv6 subnet for client use>'
-uci add_list network.wg0.addresses='<An IPv4 subnet for client use, in CIDR notation>'
+uci add_list network.wg0.addresses='10.10.10.0/24'
 
 # Guardar los cambios
 uci commit network
