16ff4e1118d07460d12dd510802f59f50d3cc67b
NiPeGun <nipegun@gmail.com>
2021-03-17 17:06:48 +0100

    Update OpenWrt-ActualizarDistro.sh

--- OpenWrt-ActualizarDistro.sh
+++ OpenWrt-ActualizarDistro.sh
@@ -13,6 +13,7 @@
 /root/scripts/o-scripts/OpenWrt-Actualizar.sh
 
 # Determinar cual es la última versión de OpenWrt
+#$UltVersOpenWrt=$(curl --silent https://downloads.openwrt.org/releases/ | grep -B 1 faillogs | grep -v faillogs | cut -d '"' -f 4)
 $UltVersOpenWrt=$(curl --silent https://downloads.openwrt.org/releases/ | grep -B 1 faillogs | grep -v faillogs | cut -d '"' -f 4 | sed 's/.$//')
 
 echo ""
