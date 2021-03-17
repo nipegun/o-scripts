465410dbaeae4abe29c071b8ade4c60dd7571956
NiPeGun <nipegun@gmail.com>
2021-03-17 17:34:03 +0100

    Update OpenWrt-ActualizarDistro.sh

--- OpenWrt-ActualizarDistro.sh
+++ OpenWrt-ActualizarDistro.sh
@@ -29,3 +29,20 @@ curl --silent -R -O https://downloads.openwrt.org/releases/$UltVersOpenWrt/targe
 opkg install /root/paquetes/kernel/$PaqueteKernel
 
 # Determinar la versión instalada de la distro
+VersInsalada=$(cat  /etc/opkg/distfeeds.conf | grep base | cut -d '/' -f 6)
+
+# Cambiar repos a la última versión
+sed -i.1 's/$VersInsalada/$UltVersOpenWrt/g' /etc/opkg/distfeeds.conf
+opkg update
+
+# Actualizar el paquete base-files
+  # Quitar el permiso de ejecución al scripts de funciones para que el script de base-files no pueda ejecutarlo
+  chmod -x /lib/functions.sh
+  opkg upgrade base-files
+
+# Volver a indicar el servidor DNS
+echo "nameserver 127.0.0.1" > /etc/resolv.conf
+
+# Volver a ejecutar el script de actualización de paquetes para que se actualicen todos a la última versión
+/root/scripts/o-scripts/OpenWrt-Actualizar.sh
+
