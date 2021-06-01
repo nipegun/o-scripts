a79c1c9b3babcd6a7c877b3882ed420b6ed82104
NiPeGun <nipegun@gmail.com>
2021-06-01 14:32:07 +0200

    Create PartOVMF-Montar.sh

---
+++ PartOVMF-Montar.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+#-------------------------------------------------------------
+#  Script de NiPeGun para montar la partición EFI de OpenWrt
+#-------------------------------------------------------------
+
+mkdir -p /PartOVMF/ 2> /dev/null
+mount -t auto /dev/sda1 /PartOVMF/
+
