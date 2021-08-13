b762f19e08459645cba6bc672c571c6081e1c209
NiPeGun <nipegun@gmail.com>
2021-08-13 16:07:53 +0200

    Create WireGuard-Ver-Estado.sh

---
+++ WireGuard-Ver-Estado.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+#--------------------------------------------------------------------------------------
+#  Script de NiPeGun para mostrar el estado de WireGuard desde la terminal de OpenWrt
+#--------------------------------------------------------------------------------------
+
+mkdir -p /CopSegInt/WireGuard
