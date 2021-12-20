2afef852bde6a3a41e003631569d5f9751f2e39b
NiPeGun <nipegun@gmail.com>
2021-12-20 18:50:00 +0100

    Create DNS-EstablecerServidores.sh

---
+++ DNS-EstablecerServidores.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+#--------------------------------------------------------
+#  Script de NiPeGun para establecer los servidores DNS
+#
+#  Ejecución remota:
+#  curl -s | sh
+#
+#---------------------------------------------------------
+
+echo ""
+echo "  Quitando el atributo ininmutable a /etc/resolv.conf ..."
+echo ""
+chattr -i /etc/resolv.conf
+
+echo ""
+echo "  Estableciendo el/los servidor/es DNS..."
+echo ""
+echo "nameserver 1.1.1.1" > /etc/resolv.conf
+
+echo ""
+echo "  Volviendo a agregar el atributo ininmutable a /etc/resolv.conf ..."
+echo ""
+chattr +i /etc/resolv.conf
+
+echo ""
+echo "  El contenido final de archivo /etc/resolv.conf es:"
+echo ""
+cat /etc/resolv.conf
+
