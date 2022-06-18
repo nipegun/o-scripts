d1b8e72201662696096c79228edf68888b91ef3e
NiPeGun <nipegun@gmail.com>
2022-06-18 03:14:10 +0200

    Update ConfigurarOpenWrt21ComoMVdeProxmox.sh

--- PostInst/ConfigurarOpenWrt21ComoMVdeProxmox.sh
+++ PostInst/ConfigurarOpenWrt21ComoMVdeProxmox.sh
@@ -17,98 +17,163 @@ echo -e "${ColorVerde}Configurando OpenWrt 21 como máquina virtual de Proxmox..
 echo ""
 
 # Configurar red e interfaces
-
-echo "config interface 'loopback'"                  > /etc/config/network
-echo "  option proto 'static'"                     >> /etc/config/network
-echo "  option ipaddr '127.0.0.1'"                 >> /etc/config/network
-echo "  option netmask '255.0.0.0'"                >> /etc/config/network
-echo "  option device 'lo'"                        >> /etc/config/network
-echo ""                                            >> /etc/config/network
-echo "config interface 'i_wan'"                    >> /etc/config/network
-echo "  option proto 'static'"                     >> /etc/config/network
-echo "  option gateway '192.168.1.1'"              >> /etc/config/network
-echo "  option ipaddr '192.168.1.201'"             >> /etc/config/network
-echo "  option netmask '255.255.255.0'"            >> /etc/config/network
-echo "  list dns '1.1.1.1'"                        >> /etc/config/network
-echo "  option device 'eth0'"                      >> /etc/config/network
-echo ""                                            >> /etc/config/network
-echo "config interface 'i_lan'"                    >> /etc/config/network
-echo "  option proto 'static'"                     >> /etc/config/network
-echo "  option ipaddr '192.168.2.1'"               >> /etc/config/network
-echo "  option netmask '255.255.255.0'"            >> /etc/config/network
-echo "  list dns '1.1.1.1'"                        >> /etc/config/network
-echo "  option delegate '0'"                       >> /etc/config/network
-echo "  option force_link '0'"                     >> /etc/config/network
-echo "  option device 'br_lan'"                    >> /etc/config/network
-echo ""                                            >> /etc/config/network
-echo "config device"                               >> /etc/config/network
-echo "  option name 'br_lan'"                      >> /etc/config/network
-echo "  option type 'bridge'"                      >> /etc/config/network
-echo "  list ports 'eth1'"                         >> /etc/config/network
-echo "  list ports 'eth2'"                         >> /etc/config/network
-echo "  list ports 'eth3'"                         >> /etc/config/network
-echo "  list ports 'eth4'"                         >> /etc/config/network
+  echo "config interface 'loopback'"                  > /etc/config/network
+  echo "  option proto 'static'"                     >> /etc/config/network
+  echo "  option ipaddr '127.0.0.1'"                 >> /etc/config/network
+  echo "  option netmask '255.0.0.0'"                >> /etc/config/network
+  echo "  option device 'lo'"                        >> /etc/config/network
+  echo ""                                            >> /etc/config/network
+  echo "config interface 'i_wan'"                    >> /etc/config/network
+  echo "  option proto 'static'"                     >> /etc/config/network
+  echo "  option gateway '192.168.1.1'"              >> /etc/config/network
+  echo "  option ipaddr '192.168.1.201'"             >> /etc/config/network
+  echo "  option netmask '255.255.255.0'"            >> /etc/config/network
+  echo "  list dns '1.1.1.1'"                        >> /etc/config/network
+  echo "  option device 'eth0'"                      >> /etc/config/network
+  echo ""                                            >> /etc/config/network
+  echo "config interface 'i_lan'"                    >> /etc/config/network
+  echo "  option proto 'static'"                     >> /etc/config/network
+  echo "  option ipaddr '192.168.2.1'"               >> /etc/config/network
+  echo "  option netmask '255.255.255.0'"            >> /etc/config/network
+  echo "  list dns '1.1.1.1'"                        >> /etc/config/network
+  #echo "  option delegate '0'"                       >> /etc/config/network
+  #echo "  option force_link '0'"                     >> /etc/config/network
+  echo "  option device 'br_lan'"                    >> /etc/config/network
+  echo ""                                            >> /etc/config/network
+  echo "config interface 'i_iot'"                    >> /etc/config/network
+  echo "  option proto 'static'"                     >> /etc/config/network
+  echo "  option ipaddr '192.168.254.1'"               >> /etc/config/network
+  echo "  option netmask '255.255.255.0'"            >> /etc/config/network
+  echo "  list dns '1.1.1.1'"                        >> /etc/config/network
+ # echo "  option delegate '0'"                       >> /etc/config/network
+ # echo "  option force_link '0'"                     >> /etc/config/network
+  echo "  option device 'br_iot'"                    >> /etc/config/network
+  echo ""                                            >> /etc/config/network
+  echo "config device"                               >> /etc/config/network
+  echo "  option name 'br_lan'"                      >> /etc/config/network
+  echo "  option type 'bridge'"                      >> /etc/config/network
+  echo "  list ports 'eth1'"                         >> /etc/config/network
+  echo "  list ports 'eth2'"                         >> /etc/config/network
+  echo "  list ports 'eth3'"                         >> /etc/config/network
+  echo "  list ports 'eth4'"                         >> /etc/config/network
+  echo ""                                            >> /etc/config/network
+  echo "config device"                               >> /etc/config/network
+  echo "  option name 'br_iot'"                      >> /etc/config/network
+  echo "  option type 'bridge'"                      >> /etc/config/network
+  echo "  option bridge_empty '1'"                   >> /etc/config/network
+  echo ""                                            >> /etc/config/network
 
 # Configurar cortafuegos
-
-# Permitir SSH desde WAN
-echo ""                                            >> /etc/config/firewall
-echo "config rule"                                 >> /etc/config/firewall
-echo "  option name 'Allow-SSH-WAN'"               >> /etc/config/firewall
-echo "  option target 'ACCEPT'"                    >> /etc/config/firewall
-echo "  list proto 'tcp'"                          >> /etc/config/firewall
-echo "  option dest_port '22'"                     >> /etc/config/firewall
-echo "  option src 'z_wan'"                        >> /etc/config/firewall
-# Permitir LUCI desde WAN
-echo ""                                            >> /etc/config/firewall
-echo "config rule"                                 >> /etc/config/firewall
-echo "  option name 'Allow-LUCI-WAN'"              >> /etc/config/firewall
-echo "  option target 'ACCEPT'"                    >> /etc/config/firewall
-echo "  list proto 'tcp'"                          >> /etc/config/firewall
-echo "  option dest_port '80'"                     >> /etc/config/firewall
-echo "  option src 'z_wan'"                        >> /etc/config/firewall
-
+  # defaults
+    echo "config defaults"                          > /etc/config/firewall
+    echo "  option input 'ACCEPT'"                 >> /etc/config/firewall
+    echo "  option output 'ACCEPT'"                >> /etc/config/firewall
+    echo "  option forward 'ACCEPT'"               >> /etc/config/firewall
+    echo "  option synflood_protect '1'"           >> /etc/config/firewall
+    echo "  option drop_invalid '1'"               >> /etc/config/firewall
+  # zona wan
+    echo "config zone"                             >> /etc/config/firewall
+    echo "  list network 'i_wan'"                  >> /etc/config/firewall
+    echo "  option input 'ACCEPT'"                 >> /etc/config/firewall
+    echo "  option output 'ACCEPT'"                >> /etc/config/firewall
+    echo "  option forward 'ACCEPT'"               >> /etc/config/firewall
+    echo "  option name 'z_wan'"                   >> /etc/config/firewall
+    echo "  option masq '1'"                       >> /etc/config/firewall
+    echo "  option mtu_fix '1' "                   >> /etc/config/firewall
+  # zona lan
+    echo "config zone"                             >> /etc/config/firewall
+    echo "  list network 'i_lan'"                  >> /etc/config/firewall
+    echo "  option input 'ACCEPT'"                 >> /etc/config/firewall
+    echo "  option output 'ACCEPT'"                >> /etc/config/firewall
+    echo "  option forward 'ACCEPT'"               >> /etc/config/firewall
+    echo "  option name 'z_lan'"                   >> /etc/config/firewall
+  # zona iot
+    echo "config zone"                             >> /etc/config/firewall
+    echo "  list network 'i_iot'"                  >> /etc/config/firewall
+    echo "  option input 'ACCEPT'"                 >> /etc/config/firewall
+    echo "  option output 'ACCEPT'"                >> /etc/config/firewall
+    echo "  option forward 'ACCEPT'"               >> /etc/config/firewall
+    echo "  option name 'z_iot'"                   >> /etc/config/firewall
+  # Forwarding
+    echo ""                                        >> /etc/config/firewall
+    echo "config forwarding"                       >> /etc/config/firewall
+    echo "  option src 'z_lan'"                    >> /etc/config/firewall
+    echo "  option dest 'zwan'"                    >> /etc/config/firewall
+  # Permitir DHCP desde WAN
+    echo ""                                        >> /etc/config/firewall
+    echo "config rule"                             >> /etc/config/firewall
+    echo "  list proto 'udp'"                      >> /etc/config/firewall
+    echo "  option name 'Permitir-DHCP-desde-WAN'" >> /etc/config/firewall
+    echo "  option src 'z_wan'"                    >> /etc/config/firewall
+    echo "  option family 'ipv4'"                  >> /etc/config/firewall
+    echo "  option dest_port '68'"                 >> /etc/config/firewall
+    echo "  option target 'ACCEPT'"                >> /etc/config/firewall
+  # Permitir LUCI desde WAN
+    echo ""                                        >> /etc/config/firewall
+    echo "config rule"                             >> /etc/config/firewall
+    echo "  list proto 'tcp'"                      >> /etc/config/firewall
+    echo "  option name 'Permitir-LUCI-desde-WAN'" >> /etc/config/firewall
+    echo "  option src 'z_wan'"                    >> /etc/config/firewall
+    echo "  option dest_port '80'"                 >> /etc/config/firewall
+    echo "  option target 'ACCEPT'"                >> /etc/config/firewall
+  # Permitir SSH desde WAN
+    echo ""                                        >> /etc/config/firewall
+    echo "config rule"                             >> /etc/config/firewall
+    echo "  list proto 'tcp'"                      >> /etc/config/firewall
+    echo "  option name 'Permitir-SSH-dede-WAN'"   >> /etc/config/firewall
+    echo "  option src 'z_wan'"                    >> /etc/config/firewall
+    echo "  option dest_port '22'"                 >> /etc/config/firewall
+    echo "  option target 'ACCEPT'"                >> /etc/config/firewall
+  # Permitir ping desde WAN
+    echo ""                                        >> /etc/config/firewall
+    echo "config rule"                             >> /etc/config/firewall
+    echo "  list proto 'icmp'"                     >> /etc/config/firewall
+    echo "  option name 'Permitir-Ping-desde-WAN'" >> /etc/config/firewall
+    echo "  option src 'z_wan'"                    >> /etc/config/firewall
+    echo "  option family 'ipv4'"                  >> /etc/config/firewall
+    echo "  option icmp_type 'echo-request'"       >> /etc/config/firewall
+    echo "  option target 'ACCEPT'"                >> /etc/config/firewall
 # Adblock
-
-mkdir -p /root/logs/dns/ 2> /dev/null
-echo "config adblock 'global'"                      > /etc/config/adblock
-echo "  option adb_dnsfilereset '0'"               >> /etc/config/adblock
-echo "  option adb_backup '1'"                     >> /etc/config/adblock
-echo "  option adb_maxqueue '4'"                   >> /etc/config/adblock
-echo "  option adb_dns 'dnsmasq'"                  >> /etc/config/adblock
-echo "  option adb_fetchutil 'uclient-fetch'"      >> /etc/config/adblock
-echo "  option adb_report '1'"                     >> /etc/config/adblock
-echo "  option adb_mail '1'"                       >> /etc/config/adblock
-echo "  option adb_mailreceiver 'mail@gmail.com'"  >> /etc/config/adblock
-echo "  option adb_dnsflush '1'"                   >> /etc/config/adblock
-echo "  option adb_mailtopic 'AdBlock de OpenWrt'" >> /etc/config/adblock
-echo "  option adb_debug '1'"                      >> /etc/config/adblock
-echo "  option adb_forcedns '1'"                   >> /etc/config/adblock
-echo "  list adb_sources 'adaway'"                 >> /etc/config/adblock
-echo "  list adb_sources 'adguard'"                >> /etc/config/adblock
-echo "  list adb_sources 'android_tracking'"       >> /etc/config/adblock
-echo "  list adb_sources 'anti_ad'"                >> /etc/config/adblock
-echo "  list adb_sources 'disconnect'"             >> /etc/config/adblock
-echo "  list adb_sources 'firetv_tracking'"        >> /etc/config/adblock
-echo "  list adb_sources 'malwaredomains'"         >> /etc/config/adblock
-echo "  list adb_sources 'malwarelist'"            >> /etc/config/adblock
-echo "  list adb_sources 'notracking'"             >> /etc/config/adblock
-echo "  list adb_sources 'openphish'"              >> /etc/config/adblock
-echo "  list adb_sources 'phishing_army'"          >> /etc/config/adblock
-echo "  list adb_sources 'spam404'"                >> /etc/config/adblock
-echo "  list adb_sources 'stopforumspam'"          >> /etc/config/adblock
-echo "  list adb_sources 'whocares'"               >> /etc/config/adblock
-echo "  list adb_safesearch '0'"                   >> /etc/config/adblock
-echo "  list adb_reportdir '/root/logs/dns'"       >> /etc/config/adblock
-echo "  list adb_repiface 'br_lan'"                >> /etc/config/adblock
-echo "  list adb_enabled '1'"                      >> /etc/config/adblock
+  mkdir -p /root/logs/dns/ 2> /dev/null
+  echo "config adblock 'global'"                      > /etc/config/adblock
+  echo "  option adb_dnsfilereset '0'"               >> /etc/config/adblock
+  echo "  option adb_backup '1'"                     >> /etc/config/adblock
+  echo "  option adb_maxqueue '4'"                   >> /etc/config/adblock
+  echo "  option adb_dns 'dnsmasq'"                  >> /etc/config/adblock
+  echo "  option adb_fetchutil 'uclient-fetch'"      >> /etc/config/adblock
+  echo "  option adb_report '1'"                     >> /etc/config/adblock
+  echo "  option adb_mail '1'"                       >> /etc/config/adblock
+  echo "  option adb_mailreceiver 'mail@gmail.com'"  >> /etc/config/adblock
+  echo "  option adb_dnsflush '1'"                   >> /etc/config/adblock
+  echo "  option adb_mailtopic 'AdBlock de OpenWrt'" >> /etc/config/adblock
+  echo "  option adb_debug '1'"                      >> /etc/config/adblock
+  echo "  option adb_forcedns '1'"                   >> /etc/config/adblock
+  echo "  list adb_sources 'adaway'"                 >> /etc/config/adblock
+  echo "  list adb_sources 'adguard'"                >> /etc/config/adblock
+  echo "  list adb_sources 'android_tracking'"       >> /etc/config/adblock
+  echo "  list adb_sources 'anti_ad'"                >> /etc/config/adblock
+  echo "  list adb_sources 'disconnect'"             >> /etc/config/adblock
+  echo "  list adb_sources 'firetv_tracking'"        >> /etc/config/adblock
+  echo "  list adb_sources 'malwaredomains'"         >> /etc/config/adblock
+  echo "  list adb_sources 'malwarelist'"            >> /etc/config/adblock
+  echo "  list adb_sources 'notracking'"             >> /etc/config/adblock
+  echo "  list adb_sources 'openphish'"              >> /etc/config/adblock
+  echo "  list adb_sources 'phishing_army'"          >> /etc/config/adblock
+  echo "  list adb_sources 'spam404'"                >> /etc/config/adblock
+  echo "  list adb_sources 'stopforumspam'"          >> /etc/config/adblock
+  echo "  list adb_sources 'whocares'"               >> /etc/config/adblock
+  echo "  list adb_safesearch '0'"                   >> /etc/config/adblock
+  echo "  list adb_reportdir '/root/logs/dns'"       >> /etc/config/adblock
+  echo "  list adb_repiface 'br_lan'"                >> /etc/config/adblock
+  echo "  list adb_enabled '1'"                      >> /etc/config/adblock
 
 # DHCP en LAN
+  echo "config dhcp 'LAN'"                           >> /etc/config/dhcp
+  echo "  option start '100'"                        >> /etc/config/dhcp
+  echo "  option leasetime '12h'"                    >> /etc/config/dhcp
+  echo "  option interface 'i_lan'"                  >> /etc/config/dhcp
+  echo "  option limit '199'"                        >> /etc/config/dhcp
 
-echo "config dhcp 'LAN'"                           >> /etc/config/dhcp
-echo "  option start '100'"                        >> /etc/config/dhcp
-echo "  option leasetime '12h'"                    >> /etc/config/dhcp
-echo "  option interface 'i_lan'"                  >> /etc/config/dhcp
-echo "  option limit '199'"                        >> /etc/config/dhcp
+# Apagar
+  poweroff
 
-poweroff
