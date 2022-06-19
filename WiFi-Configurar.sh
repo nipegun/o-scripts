8dd8797643f99abe43ade7cd19297ee5863d0f36
NiPeGun <nipegun@gmail.com>
2022-06-19 22:43:38 +0200

    Update WiFi-Configurar.sh

--- WiFi-Configurar.sh
+++ WiFi-Configurar.sh
@@ -58,46 +58,45 @@ echo ""
   echo "  option disabled '0'"                                >> /etc/config/wireless
   echo ""                                                     >> /etc/config/wireless
   echo "config wifi-iface 'inv_radio0'"                       >> /etc/config/wireless
-  echo "  option ifname 'wlan0_1'"                            >> /etc/config/wireless
+  echo "  option ifname 'wlan0.1'"                            >> /etc/config/wireless
   echo "  option device 'radio0'"                             >> /etc/config/wireless
   echo "  option mode 'ap'"                                   >> /etc/config/wireless
-  echo "  option ssid 'Invitadoss'"                            >> /etc/config/wireless
+  echo "  option ssid 'Invitados'"                            >> /etc/config/wireless
   echo "  option encryption 'sae-mixed'"                      >> /etc/config/wireless
   echo "  option key 'Conectar0'"                             >> /etc/config/wireless
-  echo "  option isolate '1'"                                 >> /etc/config/wireless
+  echo "  option isolate '0'"                                 >> /etc/config/wireless
   echo "  option network 'i_inv'"                             >> /etc/config/wireless
   echo "  option disabled '0'"                                >> /etc/config/wireless
   echo ""                                                     >> /etc/config/wireless
   echo "config wifi-iface 'inv_radio1'"                       >> /etc/config/wireless
-  echo "  option ifname 'wlan1_1'"                            >> /etc/config/wireless
+  echo "  option ifname 'wlan1.1'"                            >> /etc/config/wireless
   echo "  option device 'radio1'"                             >> /etc/config/wireless
   echo "  option mode 'ap'"                                   >> /etc/config/wireless
-  echo "  option ssid 'Invitadoss'"                            >> /etc/config/wireless
+  echo "  option ssid 'Invitados'"                            >> /etc/config/wireless
   echo "  option encryption 'sae-mixed'"                      >> /etc/config/wireless
   echo "  option key 'Conectar0'"                             >> /etc/config/wireless
-  echo "  option isolate '1'"                                 >> /etc/config/wireless
+  echo "  option isolate '0'"                                 >> /etc/config/wireless
   echo "  option network 'i_inv'"                             >> /etc/config/wireless
   echo "  option disabled '0'"                                >> /etc/config/wireless
   echo ""                                                     >> /etc/config/wireless
   echo "config wifi-iface 'iot_radio0'"                       >> /etc/config/wireless
-  echo "  option ifname 'wlan0_2'"                            >> /etc/config/wireless
+  echo "  option ifname 'wlan0.2'"                            >> /etc/config/wireless
   echo "  option device 'radio0'"                             >> /etc/config/wireless
   echo "  option mode 'ap'"                                   >> /etc/config/wireless
   echo "  option ssid 'IoT'"                                  >> /etc/config/wireless
   echo "  option encryption 'sae-mixed'"                      >> /etc/config/wireless
   echo "  option key 'Conectar0'"                             >> /etc/config/wireless
-  echo "  option isolate '1'"                                 >> /etc/config/wireless
+  echo "  option isolate '0'"                                 >> /etc/config/wireless
   echo "  option network 'i_iot'"                             >> /etc/config/wireless
   echo "  option disabled '0'"                                >> /etc/config/wireless
   echo ""                                                     >> /etc/config/wireless
   echo "config wifi-iface 'iot_radio1'"                       >> /etc/config/wireless
-  echo "  option ifname 'wlan1_2'"                            >> /etc/config/wireless
+  echo "  option ifname 'wlan1.2'"                            >> /etc/config/wireless
   echo "  option device 'radio1'"                             >> /etc/config/wireless
   echo "  option mode 'ap'"                                   >> /etc/config/wireless
   echo "  option ssid 'IoT'"                                  >> /etc/config/wireless
   echo "  option encryption 'sae-mixed'"                      >> /etc/config/wireless
   echo "  option key 'Conectar0'"                             >> /etc/config/wireless
-  echo "  option isolate '1'"                                 >> /etc/config/wireless
+  echo "  option isolate '0'"                                 >> /etc/config/wireless
   echo "  option network 'i_iot'"                             >> /etc/config/wireless
   echo "  option disabled '0'"                                >> /etc/config/wireless
-
