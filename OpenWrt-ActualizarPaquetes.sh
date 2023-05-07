fc933b2837a62d0e7d00ac3e20cf4a7808b70d55
NiPeGun <nipegun@gmail.com>
2023-05-07 03:06:25 +0200

    Update OpenWrt-ActualizarPaquetes.sh

--- OpenWrt-ActualizarPaquetes.sh
+++ OpenWrt-ActualizarPaquetes.sh
@@ -5,42 +5,42 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
-#-------------------------------------------------------------------------
-#  Script de NiPeGun para actualizar OpenWrt
+# ----------
+# Script de NiPeGun para actualizar OpenWrt
 #
-#  Basado en la forma oficial de actualizarlo desde la línea de comandos:
+# Basado en la forma oficial de actualizarlo desde la línea de comandos:
 #
-#  opkg update
-#  opkg list-upgradable | cut -f 1 -d ' ' | xargs opkg upgrade
-#-------------------------------------------------------------------------
+#   opkg update
+#   opkg list-upgradable | cut -f 1 -d ' ' | xargs opkg upgrade
+# ----------
 
-ColorRojo='\033[1;31m'
-ColorVerde='\033[1;32m'
-FinColor='\033[0m'
+vColorAzul="\033[0;34m"
+vColorAzulClaro="\033[1;34m"
+vColorVerde='\033[1;32m'
+vColorRojo='\033[1;31m'
+vFinColor='\033[0m'
 
-echo ""
-echo -e "${ColorVerde}-------------------------------------------------------${FinColor}"
-echo -e "${ColorVerde}  Iniciando el script de actualización de paquetes...${FinColor}"
-echo -e "${ColorVerde}-------------------------------------------------------${FinColor}"
-echo ""
+# Notificar inicio de ejecución del script
+  echo ""
+  echo -e "${vColorAzulClaro}  Iniciando el script de actualización de paquetes...${vFinColor}"
+  echo ""
 
-## Actualizar la lista de paquetes disponibles
-   opkg update
+# Actualizar la lista de paquetes disponibles
+  opkg update
 
-## Crear un archivo con la lista de los paquetes actualizables
-   opkg list-upgradable | cut -f 1 -d ' ' > /var/tmp/OpenWrt-PaquetesActualizables.list
+# Crear un archivo con la lista de los paquetes actualizables
+  opkg list-upgradable | cut -f 1 -d ' ' > /var/tmp/OpenWrt-PaquetesActualizables.list
 
-## Transformar ese archivo en un script que actualice todos esos paquetes
-   mv /var/tmp/OpenWrt-PaquetesActualizables.list /var/tmp/ActualizarOpenWrt.sh
-   chmod +x /var/tmp/ActualizarOpenWrt.sh
-   sed -i -e 's/^/opkg update \&\& opkg upgrade /' /var/tmp/ActualizarOpenWrt.sh
+# Transformar ese archivo en un script que actualice todos esos paquetes
+  mv /var/tmp/OpenWrt-PaquetesActualizables.list /var/tmp/ActualizarOpenWrt.sh
+  chmod +x /var/tmp/ActualizarOpenWrt.sh
+  sed -i -e 's/^/opkg update \&\& opkg upgrade /' /var/tmp/ActualizarOpenWrt.sh
 
-## Ejecutar el script de actualización recién creado
-   /var/tmp/ActualizarOpenWrt.sh
+# Ejecutar el script de actualización recién creado
+  /var/tmp/ActualizarOpenWrt.sh
 
-echo ""
-echo -e "${ColorVerde}----------------------------------------------------${FinColor}"
-echo -e "${ColorVerde}  Script de actualización de paquetes, finalizado.${FinColor}"
-echo -e "${ColorVerde}----------------------------------------------------${FinColor}"
-echo ""
+# Notificar fin de ejecución del script
+  echo ""
+  echo -e "${vColorVerde}  Script de actualización de paquetes, finalizado.${vFinColor}"
+  echo ""
 
