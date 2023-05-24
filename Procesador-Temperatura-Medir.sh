8de53e16fb44009e400f5d0bd098d91b5619de59
NiPeGun <nipegun@gmail.com>
2023-05-24 16:06:46 +0200

    Create Procesador-Temperatura-Medir.sh

---
+++ Procesador-Temperatura-Medir.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+# ----------
+# Script de NiPeGun para medir la temperatura de la CPU en OpenWrt
+#
+# Ejecución remota:
+#   curl -s https://raw.githubusercontent.com/nipegun/o-scripts/master/Procesador-Temperatura-Medir.sh | sh
+# ----------
+
+vTempEntero=$(cat /sys/class/thermal/thermal_zone0/temp)
+vTempCelcius=$(($vTempEntero / 1000))
+
+echo $vTempCelcius
+
