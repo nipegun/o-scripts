f78b832d01a35e2af3b6534d63c0588b998420be
NiPeGun <nipegun@gmail.com>
2023-05-25 08:36:40 +0200

    Update OScripts-CrearAlias.sh

--- OScripts-CrearAlias.sh
+++ OScripts-CrearAlias.sh
@@ -5,15 +5,19 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
-#-----------------------------------------------------------
-#  Script de NiPeGun para crear los alias de los o-scripts 
-#-----------------------------------------------------------
+# ----------
+# Script de NiPeGun para crear los alias de los o-scripts 
+# ----------
 
-ColorVerde="\033[1;32m"
-FinColor="\033[0m"
+# Definir variables de color
+  vColorAzul="\033[0;34m"
+  vColorAzulClaro="\033[1;34m"
+  vColorVerde='\033[1;32m'
+  vColorRojo='\033[1;31m'
+  vFinColor='\033[0m'
 
 echo ""
-echo -e "${ColorVerde}Creando alias para los o-scripts...${FinColor}"
+echo -e "${vColorAzulClaro}  Creando alias para los o-scripts...${vFinColor}"
 echo ""
 
 ln -s /root/scripts/o-scripts/OpenWrt-ActualizarPaquetes.sh   /root/scripts/o-scripts/Alias/aso
@@ -21,6 +25,6 @@ ln -s /root/scripts/o-scripts/OScripts-Sincronizar.sh         /root/scripts/o-sc
 ln -s /root/scripts/o-scripts/Subred-Inspeccionar.sh          /root/scripts/o-scripts/Alias/is
 
 echo ""
-echo -e "${ColorVerde}Alias creados. Deberías poder ejecutar los o-scripts escribiendo el nombre de su alias.${FinColor}"
+echo -e "${vColorVerde}    Alias creados. Deberías poder ejecutar los o-scripts escribiendo el nombre de su alias.${vFinColor}"
 echo ""
 
