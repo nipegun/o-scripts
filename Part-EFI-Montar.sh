add2da83662c71e8aa842b51b80966b66b768b74
NiPeGun <nipegun@gmail.com>
2023-05-28 22:48:46 +0200

    Update and rename PartEFI-Montar.sh to Part-EFI-Montar.sh

--- Part-EFI-Montar.sh
+++ Part-EFI-Montar.sh
@@ -5,9 +5,9 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
-#-------------------------------------------------------------
-#  Script de NiPeGun para montar la partición EFI de OpenWrt
-#-------------------------------------------------------------
+# ----------
+# Script de NiPeGun para montar la partición EFI de OpenWrt
+# ----------
 
 mkdir -p /PartEFI/ 2> /dev/null
 mount -t auto /dev/sda1 /PartEFI/
