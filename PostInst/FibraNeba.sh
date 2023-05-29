e1d80a2fa51e7d493142b5a3b001e815ff744f34
NiPeGun <nipegun@gmail.com>
2023-05-29 17:06:05 +0200

    Update FibraNeba.sh

--- PostInst/FibraNeba.sh
+++ PostInst/FibraNeba.sh
@@ -1,12 +1,19 @@
 # Jazztel
-
+  echo "config interface 'wan'"       >> /etc/config/network
+  echo "  option device 'br-wan.20'"  >> /etc/config/network
+  echo "  option proto 'dhcp'"        >> /etc/config/network
+  echo "  option hostname '*'"        >> /etc/config/network
+  echo "  option delegate '0'"        >> /etc/config/network
+  echo "  option peerdns '0'"         >> /etc/config/network
+  echo "  list dns '9.9.9.9'"         >> /etc/config/network
+  echo "  list dns '149.112.112.112'" >> /etc/config/network
 
 # Simyo
-  echo "config interface 'wan'"        >> /etc/config/network
-  echo "  option device 'br-wan.832'"  >> /etc/config/network
-  echo "  option proto 'dhcp'"         >> /etc/config/network
-  echo "  option hostname '*'"         >> /etc/config/network
-  echo "  option delegate '0'"         >> /etc/config/network
-  echo "  option peerdns '0'"          >> /etc/config/network
-  echo "  list dns '9.9.9.9'"          >> /etc/config/network
-  echo "  list dns '149.112.112.112'"  >> /etc/config/network
+  echo "config interface 'wan'"       >> /etc/config/network
+  echo "  option device 'br-wan.832'" >> /etc/config/network
+  echo "  option proto 'dhcp'"        >> /etc/config/network
+  echo "  option hostname '*'"        >> /etc/config/network
+  echo "  option delegate '0'"        >> /etc/config/network
+  echo "  option peerdns '0'"         >> /etc/config/network
+  echo "  list dns '9.9.9.9'"         >> /etc/config/network
+  echo "  list dns '149.112.112.112'" >> /etc/config/network
