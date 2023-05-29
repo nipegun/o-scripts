cf9b7156bbd47d5d799efd3db4bc5ffdb270d927
NiPeGun <nipegun@gmail.com>
2023-05-29 02:41:53 +0200

    Update Part-Overlay-Extender.sh

--- Part-Overlay-Extender.sh
+++ Part-Overlay-Extender.sh
@@ -1,11 +1,40 @@
 #!/bin/sh
 
-vTipoMemoria="mmcblk"
-vDispoYPart=$(lsblk | grep overlay | cut -d' ' -f1 | cut -d'k' -f2)
-vPartOverlay="/dev/$vTipoMemoria$vDispoYPart"
+# Determinar el dispositivo de la partición overlay actual
+  vDispPartOverlay="$(sed -n -e "/\s\/overlay\s.*$/s///p" /etc/mtab)"
 
-# Comprobar si está instalado parted
+# Configurar fstab para montar rootfs_data en otra carpeta
+  uci -q delete fstab.rwm
+  uci set fstab.rwm="mount"
+  uci set fstab.rwm.device="${vDispPartOverlay}"
+  uci set fstab.rwm.target="/rwm"
+  uci commit fstab
 
-# Mostrar información de la partición
-  parted /dev/$vPartOverlay print
+# Obtener el UUID de la que va a ser la nueva partición overlay
+  #vNuevaPartOverlay=$(blkid | grep ext4 | cut -d':' -f1 | head -n1)
+  vNuevoDispOverlay=$(blkid | grep ext4 | cut -d':' -f1 | tail -n1)
+  echo ""
+  echo "  El nuevo dispositivo overlay será $vNuevoDispOverlay."
+  echo ""
+  vNuevoUUID=$(blkid | grep $vNuevoDispOverlay | cut -d'"' -f2)
+  echo ""
+  echo "    Su UUID es $vNuevoUUID."
+  echo ""
+
+# Borrar la configuración overlay actual de fstab
+  uci -q delete fstab.overlay
+
+# Crear una nueva configuración overlay en fstab en base al UUID obtenido antes
+  uci set fstab.overlay="mount"
+  uci set fstab.overlay.uuid="${vNuevoUUID}"
+  uci set fstab.overlay.target="/overlay"
+  uci commit fstab
+
+# Transferir datos
+  mkdir -p /mnt/NuevoDispositivoOverlay
+  mount $vNuevoDispOverlay /mnt/NuevoDispositivoOverlay
+  tar -C /overlay -cvf - . | tar -C /mnt/NuevoDispositivoOverlay -xf -
+
+# Reiniciar
+  reboot
 
