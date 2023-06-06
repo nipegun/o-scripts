864fa2dc83d320eb5af3215cc42eb37979a8d270
NiPeGun <nipegun@gmail.com>
2023-06-06 14:51:48 +0200

    Update Configurar-BPIR3-Simyo-NEBA.sh

--- PostInst/Configurar-BPIR3-Simyo-NEBA.sh
+++ PostInst/Configurar-BPIR3-Simyo-NEBA.sh
@@ -19,49 +19,44 @@
   vColorRojo='\033[1;31m'
   vFinColor='\033[0m'
 
+# Notificar inicio de ejecución del script
+  echo ""
+  echo -e "${vColorAzulClaro}  Configurando la BPI-R3 para conectarse a una ONT de Simyo para fibra NEBA...${vFinColor}"
+  echo ""
+
 # /etc/config/network
   echo ""                                                                     > /etc/config/network
-  echo "config device"                                                       >> /etc/config/network
-  echo "  option name 'br-wan'"                                              >> /etc/config/network
-  echo "  option type 'bridge'"                                              >> /etc/config/network
-  echo "  list ports 'eth1'"                                                 >> /etc/config/network
-  echo "  list ports 'wan'"                                                  >> /etc/config/network
-  echo "  option bridge_empty '1'"                                           >> /etc/config/network
-  echo ""                                                                    >> /etc/config/network
-  echo "config device"                                                       >> /etc/config/network
-  echo "  option name 'br-lan'"                                              >> /etc/config/network
-  echo "  option type 'bridge'"                                              >> /etc/config/network
-  echo "  list ports 'lan1'"                                                 >> /etc/config/network
-  echo "  list ports 'lan2'"                                                 >> /etc/config/network
-  echo "  list ports 'lan3'"                                                 >> /etc/config/network
-  echo "  list ports 'lan4'"                                                 >> /etc/config/network
-  echo "  list ports 'sfp2'"                                                 >> /etc/config/network
-  echo "  option bridge_empty '1'"                                           >> /etc/config/network
-  echo ""                                                                    >> /etc/config/network
-  echo "config device"                                                       >> /etc/config/network
-  echo "  option name 'br-iot'"                                              >> /etc/config/network
-  echo "  option type 'bridge'"                                              >> /etc/config/network
-  echo "  option bridge_empty '1'"                                           >> /etc/config/network
-  echo ""                                                                    >> /etc/config/network
-  echo "config device"                                                       >> /etc/config/network
-  echo "  option name 'br-inv'"                                              >> /etc/config/network
-  echo "  option type 'bridge'"                                              >> /etc/config/network
-  echo "  option bridge_empty '1'"                                           >> /etc/config/network
-  echo ""                                                                    >> /etc/config/network
   echo "config interface 'loopback'"                                         >> /etc/config/network
   echo "  option device 'lo'"                                                >> /etc/config/network
   echo "  option proto 'static'"                                             >> /etc/config/network
   echo "  option ipaddr '127.0.0.1'"                                         >> /etc/config/network
   echo "  option netmask '255.0.0.0'"                                        >> /etc/config/network
   echo ""                                                                    >> /etc/config/network
+  echo "config device"                                                       >> /etc/config/network
+  echo "  option name 'br-wan'"                                              >> /etc/config/network
+  echo "  option type 'bridge'"                                              >> /etc/config/network
+  echo "  option bridge_empty '1'"                                           >> /etc/config/network
+  echo "  list ports 'eth1'"                                                 >> /etc/config/network
+  echo "  list ports 'wan'"                                                  >> /etc/config/network
+  echo ""                                                                    >> /etc/config/network
   echo "config interface 'wan'"                                              >> /etc/config/network
   echo "  option device 'br-wan.832'"                                        >> /etc/config/network
   echo "  option proto 'dhcp'"                                               >> /etc/config/network
   echo "  option hostname '*'"                                               >> /etc/config/network
-  echo "  option delegate '0'"                                               >> /etc/config/network
   echo "  option peerdns '0'"                                                >> /etc/config/network
+  echo "  option delegate '0'"                                               >> /etc/config/network
   echo "  list dns '9.9.9.9'"                                                >> /etc/config/network
   echo "  list dns '149.112.112.112'"                                        >> /etc/config/network
+  echo ""                                                                    >> /etc/config/network
+  echo "config device"                                                       >> /etc/config/network
+  echo "  option name 'br-lan'"                                              >> /etc/config/network
+  echo "  option type 'bridge'"                                              >> /etc/config/network
+  echo "  option bridge_empty '1'"                                           >> /etc/config/network
+  echo "  list ports 'lan1'"                                                 >> /etc/config/network
+  echo "  list ports 'lan2'"                                                 >> /etc/config/network
+  echo "  list ports 'lan3'"                                                 >> /etc/config/network
+  echo "  list ports 'lan4'"                                                 >> /etc/config/network
+  echo "  list ports 'sfp2'"                                                 >> /etc/config/network
   echo ""                                                                    >> /etc/config/network
   echo "config interface 'lan'"                                              >> /etc/config/network
   echo "  option device 'br-lan'"                                            >> /etc/config/network
@@ -71,21 +66,31 @@
   echo "  option delegate '0'"                                               >> /etc/config/network
   echo "  list dns '192.168.1.1'"                                            >> /etc/config/network
   echo ""                                                                    >> /etc/config/network
+  echo "config device"                                                       >> /etc/config/network
+  echo "  option name 'br-iot'"                                              >> /etc/config/network
+  echo "  option type 'bridge'"                                              >> /etc/config/network
+  echo "  option bridge_empty '1'"                                           >> /etc/config/network
+  echo ""                                                                    >> /etc/config/network
   echo "config interface 'iot'"                                              >> /etc/config/network
   echo "  option proto 'static'"                                             >> /etc/config/network
   echo "  option ipaddr '192.168.2.1'"                                       >> /etc/config/network
   echo "  option netmask '255.255.255.0'"                                    >> /etc/config/network
   echo "  option device 'br-iot'"                                            >> /etc/config/network
-  echo "  list dns '192.168.2.1'"                                            >> /etc/config/network
   echo "  option delegate '0'"                                               >> /etc/config/network
+  echo "  list dns '192.168.2.1'"                                            >> /etc/config/network
+  echo ""                                                                    >> /etc/config/network
+  echo "config device"                                                       >> /etc/config/network
+  echo "  option name 'br-inv'"                                              >> /etc/config/network
+  echo "  option type 'bridge'"                                              >> /etc/config/network
+  echo "  option bridge_empty '1'"                                           >> /etc/config/network
   echo ""                                                                    >> /etc/config/network
   echo "config interface 'inv'"                                              >> /etc/config/network
   echo "  option proto 'static'"                                             >> /etc/config/network
   echo "  option ipaddr '192.168.3.1'"                                       >> /etc/config/network
   echo "  option netmask '255.255.255.0'"                                    >> /etc/config/network
   echo "  option device 'br-inv'"                                            >> /etc/config/network
-  echo "  list dns '192.168.3.1'"                                            >> /etc/config/network
   echo "  option delegate '0'"                                               >> /etc/config/network
+  echo "  list dns '192.168.3.1'"                                            >> /etc/config/network
   echo ""                                                                    >> /etc/config/network
   echo "config interface 'vpn'"                                              >> /etc/config/network
   echo "  option proto 'wireguard'"                                          >> /etc/config/network
@@ -332,6 +337,30 @@
   echo "  option cell_density '0'"                    >> /etc/config/wireless
   echo "  option country 'ES'"                        >> /etc/config/wireless
   echo ""                                             >> /etc/config/wireless
+  echo "config wifi-iface 'wifiradio0_1'"             >> /etc/config/wireless
+  echo "  option device 'radio0'"                     >> /etc/config/wireless
+  echo "  option network 'lan'"                       >> /etc/config/wireless
+  echo "  option mode 'ap'"                           >> /etc/config/wireless
+  echo "  option ssid 'OpenWrt'"                      >> /etc/config/wireless
+  echo "  option encryption 'sae-mixed'"              >> /etc/config/wireless
+  echo "  option key 'P@ssw0rd'"                      >> /etc/config/wireless
+  echo ""                                             >> /etc/config/wireless
+  echo "config wifi-iface 'wifiradio0_2'"             >> /etc/config/wireless
+  echo "  option device 'radio0'"                     >> /etc/config/wireless
+  echo "  option mode 'ap'"                           >> /etc/config/wireless
+  echo "  option ssid 'IoT'"                          >> /etc/config/wireless
+  echo "  option encryption 'psk2'"                   >> /etc/config/wireless
+  echo "  option key 'P@ssw0rd'"                      >> /etc/config/wireless
+  echo "  option network 'iot'"                       >> /etc/config/wireless
+  echo ""                                             >> /etc/config/wireless
+  echo "config wifi-iface 'wifiradio0_3'"             >> /etc/config/wireless
+  echo "  option device 'radio0'"                     >> /etc/config/wireless
+  echo "  option mode 'ap'"                           >> /etc/config/wireless
+  echo "  option ssid 'Invitados'"                    >> /etc/config/wireless
+  echo "  option encryption 'sae-mixed'"              >> /etc/config/wireless
+  echo "  option key 'P@ssw0rd'"                      >> /etc/config/wireless
+  echo "  option network 'inv'"                       >> /etc/config/wireless
+  echo ""                                             >> /etc/config/wireless
   echo "config wifi-device 'radio1'"                  >> /etc/config/wireless
   echo "  option type 'mac80211'"                     >> /etc/config/wireless
   echo "  option path 'platform/soc/18000000.wmac+1'" >> /etc/config/wireless
@@ -341,15 +370,7 @@
   echo "  option cell_density '0'"                    >> /etc/config/wireless
   echo "  option country 'ES'"                        >> /etc/config/wireless
   echo ""                                             >> /etc/config/wireless
-  echo "config wifi-iface 'default_radio0'"           >> /etc/config/wireless
-  echo "  option device 'radio0'"                     >> /etc/config/wireless
-  echo "  option network 'lan'"                       >> /etc/config/wireless
-  echo "  option mode 'ap'"                           >> /etc/config/wireless
-  echo "  option ssid 'OpenWrt'"                      >> /etc/config/wireless
-  echo "  option encryption 'sae-mixed'"              >> /etc/config/wireless
-  echo "  option key 'P@ssw0rd'"                      >> /etc/config/wireless
-  echo ""                                             >> /etc/config/wireless
-  echo "config wifi-iface 'default_radio1'"           >> /etc/config/wireless
+  echo "config wifi-iface 'wifiradio1_1'"             >> /etc/config/wireless
   echo "  option device 'radio1'"                     >> /etc/config/wireless
   echo "  option network 'lan'"                       >> /etc/config/wireless
   echo "  option mode 'ap'"                           >> /etc/config/wireless
@@ -357,15 +378,7 @@
   echo "  option encryption 'sae-mixed'"              >> /etc/config/wireless
   echo "  option key 'P@ssw0rd'"                      >> /etc/config/wireless
   echo ""                                             >> /etc/config/wireless
-  echo "config wifi-iface 'wifinet2'"                 >> /etc/config/wireless
-  echo "  option device 'radio0'"                     >> /etc/config/wireless
-  echo "  option mode 'ap'"                           >> /etc/config/wireless
-  echo "  option ssid 'IoT'"                          >> /etc/config/wireless
-  echo "  option encryption 'psk2'"                   >> /etc/config/wireless
-  echo "  option key 'P@ssw0rd'"                      >> /etc/config/wireless
-  echo "  option network 'iot'"                       >> /etc/config/wireless
-  echo ""                                             >> /etc/config/wireless
-  echo "config wifi-iface 'wifinet3'"                 >> /etc/config/wireless
+  echo "config wifi-iface 'wifiradio1_2'"             >> /etc/config/wireless
   echo "  option device 'radio1'"                     >> /etc/config/wireless
   echo "  option mode 'ap'"                           >> /etc/config/wireless
   echo "  option ssid 'IoT'"                          >> /etc/config/wireless
@@ -373,15 +386,7 @@
   echo "  option key 'P@ssw0rd'"                      >> /etc/config/wireless
   echo "  option network 'iot'"                       >> /etc/config/wireless
   echo ""                                             >> /etc/config/wireless
-  echo "config wifi-iface 'wifinet4'"                 >> /etc/config/wireless
-  echo "  option device 'radio0'"                     >> /etc/config/wireless
-  echo "  option mode 'ap'"                           >> /etc/config/wireless
-  echo "  option ssid 'Invitados'"                    >> /etc/config/wireless
-  echo "  option encryption 'sae-mixed'"              >> /etc/config/wireless
-  echo "  option key 'P@ssw0rd'"                      >> /etc/config/wireless
-  echo "  option network 'inv'"                       >> /etc/config/wireless
-  echo ""                                             >> /etc/config/wireless
-  echo "config wifi-iface 'wifinet5'"                 >> /etc/config/wireless
+  echo "config wifi-iface 'wifiradio1_3'"             >> /etc/config/wireless
   echo "  option device 'radio1'"                     >> /etc/config/wireless
   echo "  option mode 'ap'"                           >> /etc/config/wireless
   echo "  option ssid 'Invitados'"                    >> /etc/config/wireless
