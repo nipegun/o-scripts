6a8cd0f7ff03de6f90272819f381789fe3a4db8d
NiPeGun <nipegun@gmail.com>
2023-06-08 07:24:58 +0200

    Update OScripts-Sincronizar.sh

--- OScripts-Sincronizar.sh
+++ OScripts-Sincronizar.sh
@@ -18,13 +18,13 @@
 
 # Comprobar si hay conexión a Internet antes de sincronizar los o-scripts
   # Comprobar si el paquete wget está instalado. Si no lo está, instalarlo.
-    if [ "$(opkg list-installed | grep wget)" = "" ]; then
+    if [ "$(opkg list-installed | grep uclient-fetch)" = "" ]; then
       echo ""
       echo "  "
       echo -e "${vColorRojo}  El paquete wget no está instalado. Iniciando su instalación...${vFinColor}"
       echo ""
       opkg update
-      opkg install wget
+      opkg install uclient-fetch
     fi
   wget -q --timeout=20 --spider https://github.com
   if [[ $? -eq 0 ]]; then
