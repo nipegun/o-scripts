16f23ae209723abd749ba658cf2ed8a155acb965
NiPeGun <nipegun@gmail.com>
2023-08-14 02:59:52 +0200

    Update OScripts-Instalar.sh

--- OScripts-Instalar.sh
+++ OScripts-Instalar.sh
@@ -7,6 +7,9 @@
 
 # ----------
 # Script de NiPeGun para instalar los o-scripts
+#
+# Ejecución remota:
+#  curl -sL https://raw.githubusercontent.com/nipegun/o-scripts/master/OScripts-Instalar.sh | sh
 # ----------
 
 # Definir variables de color
