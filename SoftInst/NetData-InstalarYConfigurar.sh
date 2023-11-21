2d8c7d9b2469ad15c767fe2da53917013e30730d
NiPeGun <nipegun@gmail.com>
2023-11-21 22:52:05 +0100

    Update NetData-InstalarYConfigurar.sh

--- SoftInst/NetData-InstalarYConfigurar.sh
+++ SoftInst/NetData-InstalarYConfigurar.sh
@@ -5,9 +5,12 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
-#-------------------------------------------------------------------
-#  Script de NiPeGun para instalar y configurar netdata en OpenWrt
-#-------------------------------------------------------------------
+# ----------
+# Script de NiPeGun para instalar y configurar netdata en OpenWrt
+#
+# Ejecución remota:
+#   curl -sL | sh
+# ----------
 
 # Instalar paquetes
 opkg update
