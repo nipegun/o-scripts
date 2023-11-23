aa3e8b271382fdb1cbc4dae9b768f7e2678f562e
NiPeGun <nipegun@gmail.com>
2023-11-23 04:33:01 +0100

    Create Modem-SMS-Leer.sh

---
+++ Modem-SMS-Leer.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+# ----------
+# Script de NiPeGun para leer SMS mediante la terminal de OpenWrt
+#
+# Ejecución remota:
+#   curl -sL https://raw.githubusercontent.com/nipegun/o-scripts/master/Modem-SMS-Leer.sh | sh
+# ----------
+
+# Definir variables
+  vConsolaModem="/dev/ttyUSB2"
+
+# Enviar el mensaje mediante el modem
+  echo -e "ATZ\r" > $vConsolaModem
+  sleep 1
+  echo -e "AT+CMGF=1\r" > $vConsolaModem
+  sleep 1
+  echo -e "AT+CMGS=\"ALL\"\r" > $vConsolaModem
+  sleep 1
+
