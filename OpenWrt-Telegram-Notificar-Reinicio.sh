813a3afce3ab1e334660b738627a7dcceb4c4e6f
NiPeGun <nipegun@gmail.com>
2024-04-02 22:05:24 +0200

    Update OpenWrt-Telegram-Notificar-Reinicio.sh

--- OpenWrt-Telegram-Notificar-Reinicio.sh
+++ OpenWrt-Telegram-Notificar-Reinicio.sh
@@ -6,27 +6,30 @@
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
 # ----------
-#  Script de NiPeGun para notificar el reinicio de un nodo de PVE
+#  Script de NiPeGun para notificar el reinicio de un router OpenWrt
 #
-#  Este script no puede ser ejecutado remotamente. Debe ser ejecutado desde los p-scripts.
+#  Este script no puede ser ejecutado remotamente. Debe ser ejecutado desde los o-scripts.
 #
 #  El script contempla la existencia de dos archivos en dos rutas específicas:
 #    /root/scripts/Telegram/TokenDelBot.txt (Con el token del bot que enviará el mensaje)
 #    /root/scripts/Telegram/IdChat.txt      (Con el id del chat al que enviar el mensaje de Telegram)
 # ----------
 
-vFecha=$(date +%Y/%m/%d@%T)
-vHostName=$(uci get system.@system[0].hostname)
+# Obtener el hostname
+  cHostName=$(uci get system.@system[0].hostname)
 
-# Definir variables de color
-  vColorAzul="\033[0;34m"
-  vColorAzulClaro="\033[1;34m"
-  vColorVerde='\033[1;32m'
-  vColorRojo='\033[1;31m'
-  vFinColor='\033[0m'
+# Definir fecha de ejecución del script
+  cFechaEjecScript=$(date +a%Ym%md%d@%T)
+
+# Definir constantes de color
+  cColorAzul="\033[0;34m"
+  cColorAzulClaro="\033[1;34m"
+  cColorVerde='\033[1;32m'
+  cColorRojo='\033[1;31m'
+  cFinColor='\033[0m'
 
 echo ""
-echo -e "${vColorAzulClaro}  Notificando por Telegram el reinicio del router $vHostName...${vFinColor}"
+echo -e "${cColorAzulClaro}  Notificando por Telegram el reinicio del router $cHostName...${cFinColor}"
 echo ""
 
 # Guardar la IP pública en una variable
@@ -34,24 +37,24 @@ echo ""
     if [ "$(opkg list-installed | grep curl)" = "" ]; then
       echo ""
       echo "  "
-      echo -e "${vColorRojo}  El paquete curl no está instalado. Iniciando su instalación...${vFinColor}"
+      echo -e "${cColorRojo}  El paquete curl no está instalado. Iniciando su instalación...${cFinColor}"
       echo ""
       opkg update
       opkg install curl
     fi
-  vIPWAN=$(curl -s ifconfig.me)
+  cIPWAN=$(curl -s ifconfig.me)
 
 # Notificar por Telegram
   # Comprobar si vIPWAN es una IPv4
-    if [[ $vIPWAN =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
-      vTokenDelBot=$(cat /root/scripts/Telegram/TokenDelBot.txt)
-      vIdChat=$(cat /root/scripts/Telegram/IdChat.txt)
-      vMensaje="$vFecha - El router $vHostName ha terminado de reiniciarse. Su IP pública es: $vIPWAN."
-      /root/scripts/o-scripts/Telegram-EnviarTexto.sh  "$vTokenDelBot" "$vIdChat" "$vMensaje"
+    if [[ $cIPWAN =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+      cTokenDelBot=$(cat /root/scripts/Telegram/TokenDelBot.txt)
+      cIdChat=$(cat /root/scripts/Telegram/IdChat.txt)
+      cMensaje="$cFechaEjecScript - El router $cHostName ha terminado de reiniciarse. Su IP pública es: $cIPWAN."
+      /root/scripts/o-scripts/Telegram-EnviarTexto.sh  "$cTokenDelBot" "$cIdChat" "$cMensaje"
     else
-      vTokenDelBot=$(cat /root/scripts/Telegram/TokenDelBot.txt)
-      vIdChat=$(cat /root/scripts/Telegram/IdChat.txt)
-      vMensaje="$vFecha - El router $vHostName ha terminado de reiniciarse. Todavía no tiene una IP pública."
-      /root/scripts/o-scripts/Telegram-EnviarTexto.sh  "$vTokenDelBot" "$vIdChat" "$vMensaje"
+      cTokenDelBot=$(cat /root/scripts/Telegram/TokenDelBot.txt)
+      cIdChat=$(cat /root/scripts/Telegram/IdChat.txt)
+      cMensaje="$cFechaEjecScript - El router $cHostName ha terminado de reiniciarse. Todavía no tiene una IP pública."
+      /root/scripts/o-scripts/Telegram-EnviarTexto.sh  "$cTokenDelBot" "$cIdChat" "$cMensaje"
     fi
 
