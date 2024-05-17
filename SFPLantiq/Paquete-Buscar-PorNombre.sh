54bfeb347026ab0d241eecfb289f8b618c432818
NiPeGun <nipegun@gmail.com>
2024-05-17 04:46:52 +0200

    Update Paquete-Buscar-PorNombre.sh

--- SFPLantiq/Paquete-Buscar-PorNombre.sh
+++ SFPLantiq/Paquete-Buscar-PorNombre.sh
@@ -9,7 +9,7 @@
 # Script de NiPeGun para descargar los archivos correspondientes a nano en el OpenWrt del SFP Lantiq con OpenWrt 14.07
 #
 # Ejecución remota:
-#   curl -s https://raw.githubusercontent.com/nipegun/o-scripts/master/SFPLantiq/Paquete-Buscar-PorNombre.sh | sh -s NombreDelPaquete
+#   curl -sL https://raw.githubusercontent.com/nipegun/o-scripts/master/SFPLantiq/Paquete-Buscar-PorNombre.sh | sh -s NombreDelPaquete
 # ----------
 
 vOpenWrt="14.07"
