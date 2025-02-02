fe06245dc865598c2364f0e9157deb311da14eaf
NiPeGun <nipegun@gmail.com>
2025-02-02 17:28:53 +0100

    Update ArpScan-InstalarYConfigurar.sh

--- SoftInst/ArpScan-InstalarYConfigurar.sh
+++ SoftInst/ArpScan-InstalarYConfigurar.sh
@@ -6,5 +6,5 @@ opkg-install arp-scan-database
 mkdir -p /etc/arp-scan
 curl -L https://raw.githubusercontent.com/royhills/arp-scan/refs/heads/master/mac-vendor.txt -o /etc/arp-scan/mac-vendor.txt
 # Para ejecutar:
-#  arp-scan --interface=devbrwan --localnet | grep -v DUP | sort -t . -k 1,1n -k 2,2n -k 3,3n -k 4,4n
-#  arp-scan --interface=devbrwan --localnet | grep -v DUP | sort -t . -k 1,1n -k 2,2n -k 3,3n -k 4,4n
+#  arp-scan --interface=devbrwan --localnet | grep -v DUP | grep -v ackets | grep ^[0-9] | sort -t . -k 1,1n -k 2,2n -k 3,3n -k 4,4n
+#  arp-scan --interface=devbrlan --localnet | grep -v DUP | grep -v ackets | grep ^[0-9] | sort -t . -k 1,1n -k 2,2n -k 3,3n -k 4,4n
