b0bb4faa2a453a658cbdcb990a9e4c917cb4a6f8
NiPeGun <nipegun@gmail.com>
2025-02-07 10:47:45 +0100

    Update OpenPLCRuntime-Instalar.sh

--- SoftInst/OpenPLCRuntime-Instalar.sh
+++ SoftInst/OpenPLCRuntime-Instalar.sh
@@ -7,3 +7,5 @@ git clone https://github.com/thiagoralves/OpenPLC_v3.git
 cd /opt/OpenPLC_v3/
 opkg install python3
 opkg install python3-pip
+pip3 install -r requirements.txt
+sed -i -e 's-/bin/bash-/bin/sh-g' /opt/OpenPLC_v3/install.sh
