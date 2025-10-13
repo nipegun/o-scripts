cbce92e384146d25cd7d05a0eb7a047edaa6ea1f
NiPeGun <nipegun@gmail.com>
2025-10-13 21:04:12 +0200

    Update ArpScan-InstalarYConfigurar.sh

--- SoftInst/ArpScan-InstalarYConfigurar.sh
+++ SoftInst/ArpScan-InstalarYConfigurar.sh
@@ -2,7 +2,7 @@
 
 opkg update
 opkg install arp-scan
-opkg-install arp-scan-database
+opkg install arp-scan-database
 mkdir -p /etc/arp-scan
 curl -L https://raw.githubusercontent.com/royhills/arp-scan/refs/heads/master/mac-vendor.txt -o /etc/arp-scan/mac-vendor.txt
 # Para ejecutar:
