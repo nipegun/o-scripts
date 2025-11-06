df12dd0c89332aad3f215eed5b415e0cac8bcc9d
NiPeGun <nipegun@gmail.com>
2025-11-06 23:09:23 +0000

    Update ObtenerFechaEnFormatoNiPeGun.sh

--- SoftInst/_/ObtenerFechaEnFormatoNiPeGun.sh
+++ SoftInst/_/ObtenerFechaEnFormatoNiPeGun.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Script de NiPeGun para calcular la fecha en OpenWrt sacando el formato nipeguniano
+# Script de NiPeGun para calcular la fecha en OpenWrt sacando el formato nipeguniano con milisegundos (ejemplo: a2025m11d07h00m07s00ms725)
 #  Requisitos: paquete coreutils-date
 
 vFecha=$(date +"a%Ym%md%dh%Hm%Ms%S%3N" | sed 's/\([0-9]\{3\}\)$/ms\1/')
