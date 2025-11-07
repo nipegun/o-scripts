7a24f6237860d59fad475b7e2af07200c43aae31
NiPeGun <nipegun@gmail.com>
2025-11-07 01:22:09 +0000

    Update Red-Mapa-Crear-EnHTML.sh

--- Sistema/Red-Mapa-Crear-EnHTML.sh
+++ Sistema/Red-Mapa-Crear-EnHTML.sh
@@ -10,8 +10,8 @@ echo ""
 echo "Obteniendo arps-can completo de todas las interfaces con IP..."
 echo ""
 #curl -sL https://raw.githubusercontent.com/nipegun/o-scripts/refs/heads/master/Sistema/ArpScan-CompletoConHostname.sh | sh -s -- -json | tee /tmp/ArpScanCompleto.json
-curl -sL https://raw.githubusercontent.com/nipegun/o-scripts/refs/heads/master/Sistema/ArpScan-CompletoConHostname.sh -o /tmp/ArpScan-CompletoConHostname.sh
-sh /tmp/ArpScan-CompletoConHostname.sh -json | tee /tmp/ArpScanCompleto.json
+#curl -sL https://raw.githubusercontent.com/nipegun/o-scripts/refs/heads/master/Sistema/ArpScan-CompletoConHostname.sh -o /tmp/ArpScan-CompletoConHostname.sh
+sh /root/scripts/o-scripts/Sistema/ArpScan-CompletoConHostname.sh -json | tee /tmp/ArpScanCompleto.json
 echo ""
 echo "  Guardado en /tmp/ArpScanCompleto.json"
 echo ""
@@ -19,8 +19,8 @@ echo ""
 echo ""
 echo "Convirtiendo el escaneo arp a mapa de red en JSON..."
 echo ""
-curl -sL https://raw.githubusercontent.com/nipegun/o-scripts/refs/heads/master/Sistema/Red-Mapa-Crear-EnJSON.sh -o /tmp/Red-Mapa-Crear-EnJSON.sh
-sh /tmp/Red-Mapa-Crear-EnJSON.sh /tmp/ArpScanCompleto.json | tee /tmp/MapaDeRed.json
+#curl -sL https://raw.githubusercontent.com/nipegun/o-scripts/refs/heads/master/Sistema/Red-Mapa-Crear-EnJSON.sh -o /tmp/Red-Mapa-Crear-EnJSON.sh
+sh /root/scripts/o-scripts/Sistema/Red-Mapa-Crear-EnJSON.sh /tmp/ArpScanCompleto.json | tee /tmp/MapaDeRed.json
 echo ""
 echo "  Guardado en /tmp/MapaDeRed.json"
 echo ""
@@ -28,8 +28,8 @@ echo ""
 echo ""
 echo "Convirtiendo el mapa de red en JSON hacia HTML..."
 echo "" 
-curl -sL https://raw.githubusercontent.com/nipegun/o-scripts/refs/heads/master/Sistema/Red-Mapa-ConvertirJSONenHTML.sh -o /tmp/Red-Mapa-ConvertirJSONenHTML.sh
-sh /tmp/Red-Mapa-ConvertirJSONenHTML.sh /tmp/MapaDeRed.json | tee /tmp/MapaDeRed.html
+#curl -sL https://raw.githubusercontent.com/nipegun/o-scripts/refs/heads/master/Sistema/Red-Mapa-ConvertirJSONenHTML.sh -o /tmp/Red-Mapa-ConvertirJSONenHTML.sh
+sh /root/scripts/o-scripts/Sistema/Red-Mapa-ConvertirJSONenHTML.sh /tmp/MapaDeRed.json | tee /tmp/MapaDeRed.html
 echo ""
 echo "  Guardado en /tmp/MapaDeRed.html"
 echo ""
