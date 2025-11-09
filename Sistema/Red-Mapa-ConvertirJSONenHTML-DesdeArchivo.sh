3ac8252aeb088fc65d00b2465a285b332e819b73
NiPeGun <nipegun@gmail.com>
2025-11-09 18:32:37 +0100

    Update Red-Mapa-ConvertirJSONenHTML-DesdeArchivo.sh

--- Sistema/Red-Mapa-ConvertirJSONenHTML-DesdeArchivo.sh
+++ Sistema/Red-Mapa-ConvertirJSONenHTML-DesdeArchivo.sh
@@ -39,7 +39,7 @@ cat <<'EOF'
 </style>
 </head>
 <body>
-<h1>Informe de dispositivos conectados por interfaz</h1>
+<h1>Informe de dispositivos conectados por interfaz ($(date +"a%Y,m%m,d%dh%Hm%Ms%S"))</h1>
 EOF
 
 # Parseo simple del JSON
