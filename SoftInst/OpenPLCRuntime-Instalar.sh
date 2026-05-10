704ec8001e49a26ed947846102b6df2bb6280aa4
NiPeGun <nipegun@gmail.com>
2026-05-10 18:26:22 +0200

    Update OpenPLCRuntime-Instalar.sh

--- SoftInst/OpenPLCRuntime-Instalar.sh
+++ SoftInst/OpenPLCRuntime-Instalar.sh
@@ -1,11 +1,18 @@
+#1/bin/sh
+
+# Script de NiPeGun para instalar OpenPLCRuntime directamente en OpenWrt
+#
+# Ejecución remota
+#  cusl -sL https://raw.githubusercontent.com/nipegun/o-scripts/refs/heads/master/SoftInst/OpenPLCRuntime-Instalar.sh
+
 mkdir -p /opt
 cd /opt/
 rm -rf /opt/OpenPLCRuntime
-opkg update
-opkg install git-http
+apk update
+apk add git-http
 git clone https://github.com/thiagoralves/OpenPLC_v3.git
 cd /opt/OpenPLC_v3/
-opkg install python3
-opkg install python3-pip
+apk add python3
+apk add python3-pip
 pip3 install -r requirements.txt
 sed -i -e 's-/bin/bash-/bin/sh-g' /opt/OpenPLC_v3/install.sh
