8ae749d0467fe268ec0a13dc2353d0c1200e5c8c
NiPeGun <nipegun@gmail.com>
2026-05-10 18:28:45 +0200

    Update Samba-InstalarYConfigurar.sh

--- SoftInst/Samba-InstalarYConfigurar.sh
+++ SoftInst/Samba-InstalarYConfigurar.sh
@@ -6,29 +6,29 @@
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
 # ----------
-# Script de NiPeGun para instalar y configurar netdata en OpenWrt
+# Script de NiPeGun para instalar y configurar Samba en OpenWrt
 #
 # Ejecución remota:
-#   curl -sL https://raw.githubusercontent.com/nipegun/o-scripts/master/SoftInst/NetData-InstalarYConfigurar.sh | sed 's-usuariox-NuevoNombreDeUsuario-g' | sh 
+#   curl -sL https://raw.githubusercontent.com/nipegun/o-scripts/refs/heads/master/SoftInst/Samba-InstalarYConfigurar.sh | sed 's-usuariox-NuevoNombreDeUsuario-g' | sh 
 # ----------
 
 # Actualizar la lista de paquetes disponibles en los repositorios
   echo ""
   echo "  Actualizando la lista de paquetes disponibles en los repositorios..."
   echo ""
-  opkg update
+  apk update
 
 # Instalar los paquetes de Samba
   echo ""
   echo "  Instalando los paquetes de Samba..."
   echo ""
-  opkg install luci-i18n-samba4-es
+  apk add luci-i18n-samba4-es
 
 # Instalar el paquete para agregar usuarios al sistema
   echo ""
   echo "  Instalando el paquete para agregar usuarios al sistema..."
   echo ""
-  opkg install shadow-useradd
+  apk add shadow-useradd
 
 # Agregar el primer usuario
   echo ""
