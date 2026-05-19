daf59d86d7d045f13a9ef212f7dc56e014863392
NiPeGun <nipegun@gmail.com>
2026-05-19 23:51:05 +0200

    Update LXC-Contenedor-Crear-4-Alpine-apache2.sh

--- SoftInst/LXC-Contenedor-Crear-4-Alpine-apache2.sh
+++ SoftInst/LXC-Contenedor-Crear-4-Alpine-apache2.sh
@@ -3,49 +3,59 @@
 # Ejecución remota:
 #  curl -sL https://raw.githubusercontent.com/nipegun/o-scripts/refs/heads/master/SoftInst/LXC-Contenedor-Crear-4-Alpine-apache2.sh | sh
 
-vCarpetaLXC='/mnt/nvme/lxc'
+cCarpetaLXC='/mnt/nvme/lxc'
 
-vNombreDelContenedor='alpine-apache2'
-vAlpineVers='3.23'
-vAlpineArch='arm64'
+cNombreDelContenedor='alpine-apache2'
+cAlpineVers='3.23'
+cAlpineArch='arm64'
 
 # Crear el contenedor con la última rama estable de Alpine
-  lxc-stop -n "$vNombreDelContenedor"
-  rm -rf "$vCarpetaLXC"/containers/"$vNombreDelContenedor"
-  lxc-create -n "$vNombreDelContenedor" -t download -- --dist alpine --release "$vAlpineVers" --arch "$vAlpineArch"
+  echo ''
+  echo '  ### Creando el contenedor'
+  lxc-stop -n "$cNombreDelContenedor"
+  rm -rf "$cCarpetaLXC"/containers/"$cNombreDelContenedor"
+  lxc-create -n "$cNombreDelContenedor" -t download -- --dist alpine --release "$cAlpineVers" --arch "$cAlpineArch"
+
+# Configurar auto-inicio del contendor
+  echo ''
+  echo '  ### Configurando el auto-inicio del contenedor'
+  uci add lxc-auto container
+  uci set lxc-auto.@container[-1].name="$cNombreDelContenedor"
+  uci set lxc-auto.@container[-1].timeout='60'
+  uci commit lxc-auto
 
 # Configurar la red del contenedor en openwrt
-  echo 'lxc.net.0.ipv4.address = 10.10.4.4/24' >> /mnt/nvme/lxc/containers/"$vNombreDelContenedor"/config
-  echo 'lxc.net.0.ipv4.gateway = 10.10.4.1'    >> /mnt/nvme/lxc/containers/"$vNombreDelContenedor"/config
+  echo 'lxc.net.0.ipv4.address = 10.10.4.4/24' >> /mnt/nvme/lxc/containers/"$cNombreDelContenedor"/config
+  echo 'lxc.net.0.ipv4.gateway = 10.10.4.1'    >> /mnt/nvme/lxc/containers/"$cNombreDelContenedor"/config
 # Configurar la red del contenedor en el propio Alpine
-  echo 'auto lo'                                      > "$vCarpetaLXC"/containers/"$vNombreDelContenedor"/rootfs/etc/network/interfaces
-  echo 'iface lo inet loopback'                      >> "$vCarpetaLXC"/containers/"$vNombreDelContenedor"/rootfs/etc/network/interfaces
-  echo ''                                            >> "$vCarpetaLXC"/containers/"$vNombreDelContenedor"/rootfs/etc/network/interfaces
-  echo 'auto eth0'                                   >> "$vCarpetaLXC"/containers/"$vNombreDelContenedor"/rootfs/etc/network/interfaces
-  echo 'iface eth0 inet static'                      >> "$vCarpetaLXC"/containers/"$vNombreDelContenedor"/rootfs/etc/network/interfaces
-  echo '  address 10.10.4.4'                         >> "$vCarpetaLXC"/containers/"$vNombreDelContenedor"/rootfs/etc/network/interfaces
-  echo '  netmask 255.255.255.0'                     >> "$vCarpetaLXC"/containers/"$vNombreDelContenedor"/rootfs/etc/network/interfaces
-  echo '  gateway 10.10.4.1'                         >> "$vCarpetaLXC"/containers/"$vNombreDelContenedor"/rootfs/etc/network/interfaces
+  echo 'auto lo'                                      > "$cCarpetaLXC"/containers/"$cNombreDelContenedor"/rootfs/etc/network/interfaces
+  echo 'iface lo inet loopback'                      >> "$cCarpetaLXC"/containers/"$cNombreDelContenedor"/rootfs/etc/network/interfaces
+  echo ''                                            >> "$cCarpetaLXC"/containers/"$cNombreDelContenedor"/rootfs/etc/network/interfaces
+  echo 'auto eth0'                                   >> "$cCarpetaLXC"/containers/"$cNombreDelContenedor"/rootfs/etc/network/interfaces
+  echo 'iface eth0 inet static'                      >> "$cCarpetaLXC"/containers/"$cNombreDelContenedor"/rootfs/etc/network/interfaces
+  echo '  address 10.10.4.4'                         >> "$cCarpetaLXC"/containers/"$cNombreDelContenedor"/rootfs/etc/network/interfaces
+  echo '  netmask 255.255.255.0'                     >> "$cCarpetaLXC"/containers/"$cNombreDelContenedor"/rootfs/etc/network/interfaces
+  echo '  gateway 10.10.4.1'                         >> "$cCarpetaLXC"/containers/"$cNombreDelContenedor"/rootfs/etc/network/interfaces
 # Corregir /etc/resolv.conf
-  lxc-start -n "$vNombreDelContenedor"
-  rm -f                            "$vCarpetaLXC"/containers/"$vNombreDelContenedor"/rootfs/etc/resolv.conf
-  echo "nameserver 9.9.9.9"      > "$vCarpetaLXC"/containers/"$vNombreDelContenedor"/rootfs/etc/resolv.conf
+  lxc-start -n "$cNombreDelContenedor"
+  rm -f                            "$cCarpetaLXC"/containers/"$cNombreDelContenedor"/rootfs/etc/resolv.conf
+  echo "nameserver 9.9.9.9"      > "$cCarpetaLXC"/containers/"$cNombreDelContenedor"/rootfs/etc/resolv.conf
 # Preparar contenedor
-  lxc-attach -n "$vNombreDelContenedor" -- apk update
-  lxc-attach -n "$vNombreDelContenedor" -- apk add openssh-server
-  lxc-attach -n "$vNombreDelContenedor" -- apk add curl
-  lxc-attach -n "$vNombreDelContenedor" -- apk add nano
+  lxc-attach -n "$cNombreDelContenedor" -- apk update
+  lxc-attach -n "$cNombreDelContenedor" -- apk add openssh-server
+  lxc-attach -n "$cNombreDelContenedor" -- apk add curl
+  lxc-attach -n "$cNombreDelContenedor" -- apk add nano
 # Instalar apache2
   # Instalar el paquete
-    lxc-attach -n "$vNombreDelContenedor" -- apk add apache2
+    lxc-attach -n "$cNombreDelContenedor" -- apk add apache2
   # Iniciar el servicio
-    lxc-attach -n "$vNombreDelContenedor" -- rc-service apache2 start
+    lxc-attach -n "$cNombreDelContenedor" -- rc-service apache2 start
   # Hacer que se inicie al arranque
-    lxc-attach -n "$vNombreDelContenedor" -- rc-update add apache2 default
+    lxc-attach -n "$cNombreDelContenedor" -- rc-update add apache2 default
   # Mostrar la versión instalada
-    lxc-attach -n "$vNombreDelContenedor" -- apk list -I apache2
+    lxc-attach -n "$cNombreDelContenedor" -- apk list -I apache2
   # Apagar el contenedor
-    lxc-stop -n "$vNombreDelContenedor"
+    lxc-stop -n "$cNombreDelContenedor"
     
 # Mostrar mensaje para fin de la instalación
   echo ''
@@ -53,8 +63,8 @@ vAlpineArch='arm64'
   echo '    La raíz de las páginas webs quedan en:'
   echo '      /var/www/localhost/htdocs/'
   echo '    Para iniciar el contenedor'
-  echo "      lxc-start -n "$vNombreDelContenedor" "
+  echo "      lxc-start -n "$cNombreDelContenedor" "
   echo '    Para entrar en su terminal:'
-  echo "      lxc-attach -n "$vNombreDelContenedor" -- /bin/sh"
+  echo "      lxc-attach -n "$cNombreDelContenedor" -- /bin/sh"
   echo ''
 
