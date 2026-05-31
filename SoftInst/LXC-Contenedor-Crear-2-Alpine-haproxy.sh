702c1894d57f50afaa17184bc43d6a714838e77a
NiPeGun <nipegun@gmail.com>
2026-05-31 23:01:01 +0200

    Update LXC-Contenedor-Crear-2-Alpine-haproxy.sh

--- SoftInst/LXC-Contenedor-Crear-2-Alpine-haproxy.sh
+++ SoftInst/LXC-Contenedor-Crear-2-Alpine-haproxy.sh
@@ -29,7 +29,8 @@ lxc-create -n "$cNombreDelContenedor" -t download -- --dist alpine --release "$v
   # Preparar contenedor
     lxc-attach -n "$cNombreDelContenedor" -- apk update
     lxc-attach -n "$cNombreDelContenedor" -- apk add openssh-server
-    lxc-attach -n "$cNombreDelContenedor" -- sed -i -e 's|#PermitRootLogin prohibit-password|PermitRootLogin yes|g' /etc/ssh/sshd_config
+      echo 'root:raizraiz' | lxc-attach -n "$cNombreDelContenedor" -- chpasswd
+      lxc-attach -n "$cNombreDelContenedor" -- sed -i -e 's|#PermitRootLogin prohibit-password|PermitRootLogin yes|g' /etc/ssh/sshd_config
       lxc-attach -n "$cNombreDelContenedor" -- rc-service sshd start
       lxc-attach -n "$cNombreDelContenedor" -- rc-update add sshd default
     lxc-attach -n "$cNombreDelContenedor" -- apk add curl
